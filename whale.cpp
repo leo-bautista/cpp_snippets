f5b9c0dbf4b38a08a22570cbf4ad8d1dd1b6d9e5
Leo Bautista <64666952+leo-bautista@users.noreply.github.com>
2020-09-30 16:11:17 -0400

    Create whale.cpp
    Convert an input string into a whale song

---
+++ whale.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+
+  std::string input = "hi, human\n";
+
+  std::vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+
+  std::vector<char> result;
+
+  for (int i = 0; i < input.size(); i++) {
+    for (int j = 0; j < vowels.size(); j++) {
+      if (input[i] == vowels[j]) {
+        result.push_back(vowels[j]);
+      }
+      }
+      if (input[i] == 'e' || input[i] == 'u') {
+        result.push_back(input[i]);
+    }
+  }
+
+  for (int i = 0; i < result.size(); i++){
+    std::cout << result[i];
+  }
+
+
+}
